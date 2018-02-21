b119c309b1b032ef9059803572cd19462d7f2199
Marcelo Gadotti <gadotti.engenharia@gmail.com>
2018-02-21 23:05:57 +0000

    input and output commands

--- src/main.ino
+++ src/main.ino
@@ -43,59 +43,93 @@ void init_gpio()
   pinMode(I11, INPUT);
   pinMode(I12, INPUT);
 }
+/* set value from output */
+void setOutput(uint8_t id, uint8_t val)
+{
+  // set value from output pin
+  if (val > 0) {
+    digitalWrite(id,HIGH);
+  }else
+  {
+    digitalWrite(id,LOW);
+  }
+}
+/* return value from analog pin */
+void getAnalog(uint8_t id)
+{
+  Serial.print("AD -> ");
+  Serial.println(id);
+}
+/* return value from input pin */
+void getInput(uint8_t id)
+{
+  // get value from input pin
+  Serial.println(digitalRead(id));
+}
+
 /* thread using timer one for smoothin ad values*/
 void thread_timer1(void)
 {
   digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-
 }
 
 void setup()
 {
   Serial.begin(9600);
-  Serial.println(">0.1");
   // initialize GPIO
   init_gpio();
   // create thread using timer one
   Timer1.initialize(timer_update);
   Timer1.attachInterrupt(thread_timer1);
+  Serial.println(">0.1");
 }
-char inChar[4];
+
+char inChar[8];
+char nBuffer[3];
+uint8_t idGPIO = 0, setGPIO = 0, nChar = 0;
 uint8_t index = 0;
+
 void loop()
 {
   if (Serial.available()) {
-    // get the new byte:
-    inChar[index] = (char)Serial.read();
-    Serial.print(inChar[index]);
-    index++;
-  }
-  if (index > 3) {
-    if (inChar[3] == '\n') {
-      Serial.println(">OK!");
-      switch (inChar[0]) {
+    // clear buffer
+    memset(inChar,'0',7);
+    // read char until \n
+    nChar = Serial.readBytesUntil('\n',inChar,7);
+    // Serial.println(inChar);
+    // Serial.println(nChar);
 
+    // is there a command?
+    if (inChar[0] == '>' && inChar[nChar - 1] == '#') {
+      // get id from GPIO and cast to int
+      nBuffer[0] = inChar[2];
+      nBuffer[1] = inChar[3];
+      nBuffer[2] = '\0';
+      // cast to int
+      idGPIO = (uint8_t)atoi(nBuffer);
+      // check received command
+      switch (inChar[1]) {
         case 'A':
-          Serial.println("analog");
+          // get value from analog pin
+          getAnalog(idGPIO);
           break;
-
         case 'O':
-          Serial.println("output");
+          // get id from output
+          nBuffer[0] = inChar[4];
+          nBuffer[1] = '\0';
+          setGPIO = (uint8_t)atoi(nBuffer);
+          // set output value
+          setOutput(idGPIO, setGPIO);
           break;
-
         case 'I':
-          Serial.println("input");
+          // get value from input
+          getInput(idGPIO);
           break;
-
         default:
           Serial.println("nack");
           break;
       }
-    }else
-    {
-      Serial.println("\nnack");
-    }
-    index = 0;
-  }
 
+    }
+  }
 }
